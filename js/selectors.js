ef2df4b6b68f8beb9decac8ed2f4f8eaae4dc5dc
Catherine Jimenez <xcatherine.jimenez@gmail.com>
2020-04-09 17:56:45 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -2,7 +2,4 @@
 function paragraphSelector(){
   $('p')
 }
- // use an element selector to select the p tags in index.html and
- // return those elements. (Note that we're using the plural because
- // the selector will always return an array when used with tags, even
- // if there's only one!)
+
