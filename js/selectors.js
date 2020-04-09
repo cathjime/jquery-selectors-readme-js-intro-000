38d749e57559d1e03c39cbc3b0f6a069dcbfd9fd
Catherine Jimenez <xcatherine.jimenez@gmail.com>
2020-04-09 17:55:13 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,9 +1,9 @@
 // declare your functions here...
 function paragraphSelector(){
-  
+
 }
- // 
- // use an element selector to select the p tags in index.html and 
- // return those elements. (Note that we're using the plural because 
- // the selector will always return an array when used with tags, even 
+ //
+ // use an element selector to select the p tags in index.html and
+ // return those elements. (Note that we're using the plural because
+ // the selector will always return an array when used with tags, even
  // if there's only one!)
