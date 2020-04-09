9b16db8b2eb817cc80528dc9eb72f0cbe65cb893
Catherine Jimenez <xcatherine.jimenez@gmail.com>
2020-04-09 17:55:11 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -2,3 +2,8 @@
 function paragraphSelector(){
   
 }
+ // 
+ // use an element selector to select the p tags in index.html and 
+ // return those elements. (Note that we're using the plural because 
+ // the selector will always return an array when used with tags, even 
+ // if there's only one!)
